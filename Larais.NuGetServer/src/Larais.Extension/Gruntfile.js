b5dbf64a932509ba193ad3a2607f8a6f2298da28
David Morais Ferreira <david.moraisferreira@gmail.com>
2017-02-04 00:27:45 +0100

    Skeleton for TFS Extension

---
+++ Larais.NuGetServer/src/Larais.Extension/Gruntfile.js
@@ -0,0 +1,63 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        ts: {
+            build: {
+                tsconfig: true,
+                "outDir": "scripts"
+            },
+            options: {
+                fast: 'never'
+            }
+        },
+        exec: {
+            package_dev: {
+                command: "tfx extension create --manifests vss-extension.json",
+                stdout: true,
+                stderr: true
+            },
+            package_release: {
+                command: "tfx extension create --manifests vss-extension.json",
+                stdout: true,
+                stderr: true
+            },
+            publish_dev: {
+                command: "tfx extension publish --service-url https://marketplace.visualstudio.com --manifests vss-extension.json",
+                stdout: true,
+                stderr: true
+            },
+            publish_release: {
+                command: "tfx extension publish --service-url https://marketplace.visualstudio.com --manifests vss-extension.json",
+                stdout: true,
+                stderr: true
+            }
+        },
+        copy: {
+            scripts: {
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ["node_modules/vss-web-extension-sdk/lib/VSS.SDK.min.js"],
+                    dest: "scripts",
+                    filter: "isFile"
+                }]
+            }
+        },
+
+        clean: ["scripts/**/*.js", "*.vsix", "build", "test"],
+
+    });
+
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks("grunt-exec");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask("build", ["ts:build", "copy:scripts"]);
+
+    grunt.registerTask("package-dev", ["build", "exec:package_dev"]);
+    grunt.registerTask("package-release", ["build", "exec:package_release"]);
+    grunt.registerTask("publish-dev", ["package-dev", "exec:publish_dev"]);
+    grunt.registerTask("publish-release", ["package-release", "exec:publish_release"]);
+
+    grunt.registerTask("default", ["package-dev"]);
+};
