d3b21536c110931b51fbb40615fc7acf95836a6c
David Morais Ferreira <david.moraisferreira@gmail.com>
2017-02-04 01:50:26 +0100

    Customizing grunt

--- Larais.NuGetServer/src/Larais.Extension/Gruntfile.js
+++ Larais.NuGetServer/src/Larais.Extension/Gruntfile.js
@@ -2,11 +2,9 @@
     grunt.initConfig({
         ts: {
             build: {
-                tsconfig: true,
-                "outDir": "scripts"
-            },
-            options: {
-                fast: 'never'
+                tsconfig: "tsconfig.json",
+                "outDir": "dist",
+                src: ["**/*.ts", "!node_modules/**"]
             }
         },
         exec: {
@@ -15,31 +13,10 @@
                 stdout: true,
                 stderr: true
             },
-            package_release: {
-                command: "tfx extension create --manifests vss-extension.json",
-                stdout: true,
-                stderr: true
-            },
             publish_dev: {
                 command: "tfx extension publish --service-url https://marketplace.visualstudio.com --manifests vss-extension.json",
                 stdout: true,
                 stderr: true
-            },
-            publish_release: {
-                command: "tfx extension publish --service-url https://marketplace.visualstudio.com --manifests vss-extension.json",
-                stdout: true,
-                stderr: true
-            }
-        },
-        copy: {
-            scripts: {
-                files: [{
-                    expand: true,
-                    flatten: true,
-                    src: ["node_modules/vss-web-extension-sdk/lib/VSS.SDK.min.js"],
-                    dest: "scripts",
-                    filter: "isFile"
-                }]
             }
         },
 
@@ -49,15 +26,10 @@
 
     grunt.loadNpmTasks("grunt-ts");
     grunt.loadNpmTasks("grunt-exec");
-    grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask("build", ["ts:build", "copy:scripts"]);
-
+    grunt.registerTask("build", ["ts:build"]);
     grunt.registerTask("package-dev", ["build", "exec:package_dev"]);
-    grunt.registerTask("package-release", ["build", "exec:package_release"]);
     grunt.registerTask("publish-dev", ["package-dev", "exec:publish_dev"]);
-    grunt.registerTask("publish-release", ["package-release", "exec:publish_release"]);
-
     grunt.registerTask("default", ["package-dev"]);
 };
---
+++ Larais.NuGetServer/src/Larais.Extension/src/app.ts
@@ -0,0 +1,9 @@
+﻿/// <reference types="vss-web-extension-sdk" />
+VSS.init({
+    usePlatformScripts: true,
+    usePlatformStyles: true
+});
+
+VSS.ready(function () {
+    console.log("Hi");
+});
