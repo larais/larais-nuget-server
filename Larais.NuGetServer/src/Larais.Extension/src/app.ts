b14f8fe013c754dc46d7b1c5ddc969bf4e08f9c4
David Morais Ferreira <david.moraisferreira@gmail.com>
2017-02-04 02:28:50 +0100

    Importing VSS

---
+++ Larais.NuGetServer/src/Larais.Extension/src/app.ts
@@ -0,0 +1,10 @@
+﻿/// <reference types="vss-web-extension-sdk" />
+
+VSS.init({
+    usePlatformScripts: true,
+    usePlatformStyles: true
+});
+
+VSS.ready(function () {
+    console.log("Hello World!");
+});
