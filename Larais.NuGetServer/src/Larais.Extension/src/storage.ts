87b61720b8c4c1d75785ef7b8ac4bce277238a7f
Sebastian Lang <admin@sebastian-lang.eu>
2017-02-16 18:21:10 +0100

    [Extension] storage manager, settings dialog ui tweaks

---
+++ Larais.NuGetServer/src/Larais.Extension/src/storage.ts
@@ -0,0 +1,29 @@
+﻿/// <reference types="vss-web-extension-sdk" />
+
+module Larais.Extension.Storage {
+
+    var extensionDataService: any;
+
+    export enum SettingsKey {
+        LaraisHostAddress
+    }
+
+    export function initializeStorage(): JQueryDeferred<any> {
+        var deferred = $.Deferred();
+
+        VSS.getService(VSS.ServiceIds.ExtensionData).then((dataService: any) => {
+            extensionDataService = dataService;
+            deferred.resolve();
+        });
+
+        return deferred;
+    }
+
+    export function saveValue(key: SettingsKey, value: string): IPromise<{}> {
+        return extensionDataService.setValue(SettingsKey[key], value);
+    }
+
+    export function getValue(key: SettingsKey): IPromise<{}> {
+        return extensionDataService.getValue(SettingsKey[key]);
+    }
+}
